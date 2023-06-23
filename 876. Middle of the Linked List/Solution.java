6a5cf2c38bc925da089cb67569063d3ea799ae98
immadhav31 <madhavmuralidharan123@gmail.com>
2023-06-23 09:41:34 +0530

    updated

---
+++ 876. Middle of the Linked List/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+//If we want the first middle node fast.next.next!=null
